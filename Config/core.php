cfb9cf04d02e9ccd8cd467d226487abdc94274ca
jpupo <juliopupo@hotmail.com>
2013-02-02 16:44:24 -0500

    FINANL

--- Config/core.php
+++ Config/core.php
@@ -82,6 +82,7 @@
  * Application wide charset encoding
  */
 	Configure::write('App.encoding', 'UTF-8');
+	Configure::write('Config.language', 'esp');
 
 /**
  * To configure CakePHP *not* to use mod_rewrite and to
